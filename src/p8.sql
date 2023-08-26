c379cb1829367e5054043ada4d2cc4216f320de7
twtan02 <126399779+twtan02@users.noreply.github.com>
2023-08-26 16:34:35 +0800

    Query Performance Optimization solution

--- src/p8.sql
+++ src/p8.sql
@@ -1,2 +1,4 @@
 CREATE INDEX idx_department_salary ON employees (department, salary);
 -- We want to make it faster to find employees who work in a specific department and have a salary greater than a certain amount.
+
+
